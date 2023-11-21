33acb7c590ec01f0d4047939fc57af7185dcb7da
elwyn-de-neve <elwyn_de_neve@hotmail.com>
2023-11-21 17:17:02 +0100

    new update

--- src/DogOwner.java
+++ src/DogOwner.java
@@ -8,22 +8,21 @@ public class DogOwner {
         this.sex = sex;
     }
 
-    public void changeDogName(Dog dog, String newName) {
+    public void changeDogName(String newName) {
         dog.name = newName;
     }
 
-    public void increaseDogAge(Dog dog) {
+    public void increaseDogAge() {
         dog.age++;
     }
 
     public void setDog(Dog dog) {
-        this.dog = dog;
-        // Ik had hier eerst het volgende om te checken of er al een hond was geinstantieerd, maar dit werkte niet
-        // if (this.dog == null) {
-        //     this.dog = dog;
-        // } else {
-        //     System.out.println("Een eigenaar kan maar 1 hond hebben.");
-        // }
+         if (this.dog != null) {
+             System.out.println("Een eigenaar kan maar 1 hond hebben.");
+
+         } else {
+             this.dog = dog;
+         }
     }
 
     public String toString() {
